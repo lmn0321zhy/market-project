d99c7ff016565425ca65531502855daac9c1d1ee
lmn0321zhy <liumengnan2013@163.com>
2018-08-07 20:23:36 +0800

    11

--- src/main/java/com/lmn/modules/system/user/web/UserController.java
+++ src/main/java/com/lmn/modules/system/user/web/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.*;
 import java.util.List;
 
 /**
@@ -18,6 +19,24 @@ public class UserController {
     @RequestMapping("/queryList")
     public List queryList(){
         return userService.queryList();
+    }
+    @RequestMapping("/demo")
+    public void demo(){
+        try {
+            File csv = new File("F:/writers.csv"); // CSV数据文件
+
+            BufferedWriter bw = new BufferedWriter(new FileWriter(csv, true)); // 附加
+            // 添加新的数据行
+            bw.write("\"李四\"" + "," + "\"1988\"" + "," + "\"1992\"");
+            bw.newLine();
+            bw.close();
+        } catch (FileNotFoundException e) {
+            // File对象的创建过程中的异常捕获
+            e.printStackTrace();
+        } catch (IOException e) {
+            // BufferedWriter在关闭对象捕捉异常
+            e.printStackTrace();
+        }
     }
 
     public UserServiceImpl getUserService() {
